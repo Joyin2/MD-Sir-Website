8ebad9c515dd2df8afa92f1a1d456823a6b7ba43
Joyin2 <joyinlaskar2000@gmail.com>
2023-05-16 10:02:32 +0530

    router

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Error = () => {
+    return (
+        <div>
+            <Link href="/">Back to home page</Link>
+        </div>
+    );
+}
+
+export default Error;
