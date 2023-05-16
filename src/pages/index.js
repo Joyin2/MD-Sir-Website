728c364c5af1321df773ec46217f02b5481380cf
Joyin2 <joyinlaskar2000@gmail.com>
2023-05-16 10:02:32 +0530

    router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 const index = () => {
   return (
     <div>
-      
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+          <a href="/about">about</a>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default index;
